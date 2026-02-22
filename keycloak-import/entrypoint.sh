b9e6cfb2256cd3c19b41afd4ebab030bf055d40a
Kk <iankk0806@gmail.com>
2026-02-22 23:56:16 +0800

    fix: keycloak setting

--- keycloak-import/entrypoint.sh
+++ keycloak-import/entrypoint.sh
@@ -3,7 +3,10 @@ set -e
 
 # Replace placeholders in realm JSON with environment variables
 for f in /opt/keycloak/data/import/*.json; do
-  [ -f "$f" ] && sed -i "s|\${GRAFANA_OIDC_SECRET}|${GRAFANA_OIDC_SECRET}|g" "$f"
+  [ -f "$f" ] && sed -i \
+    -e "s|\${GRAFANA_OIDC_SECRET}|${GRAFANA_OIDC_SECRET}|g" \
+    -e "s|\${VIEWER_PASSWORD}|${VIEWER_PASSWORD}|g" \
+    "$f"
 done
 
 exec /opt/keycloak/bin/kc.sh "$@"
